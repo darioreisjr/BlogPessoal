1b215188213eafaf754e7fd9b65b6bc8f5b84aad
Dario Reis <guttyerresjunior1995@gmail.com>
2022-07-26 18:17:31 -0300

    adiciona atualização de apreendizado da pagina

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -5,7 +5,7 @@ function Home() {
   return (
     <>
       <h1 className="titulo">Dario Reis</h1>
-      <p className="subtitulo" >Desenvolvedor Full Stack</p>
+      <p className="subtitulo" >Desenvolvedor Java Full Stack</p>
       <img src="https://media-exp1.licdn.com/dms/image/D4D35AQGnTz4XFJmvtA/profile-framedphoto-shrink_200_200/0/1648510909496?e=1659463200&v=beta&t=HbyPKwRSWhrR1SZBbwk3Tqt8lovsox2BlJd830R9huk" alt="Imagem tela inicial" className="img" />
     </>
   );
