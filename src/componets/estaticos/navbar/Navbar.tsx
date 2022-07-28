4285a48987b056ad7844129ec9e1093ef3b1c2ea
Dario Reis <guttyerresjunior1995@gmail.com>
2022-07-28 19:45:18 -0300

    adiciona estilização e cores a pagina home

--- src/componets/estaticos/navbar/Navbar.tsx
+++ src/componets/estaticos/navbar/Navbar.tsx
@@ -1,41 +1,47 @@
 import React from "react";
 import { AppBar, Toolbar, Typography } from '@material-ui/core';
 import { Box } from "@mui/material";
+import './Navbar.css';
 
 function Navbar() {
   return (
     <>
-      <AppBar position="static">
-        <Toolbar variant="dense">
-          <Box style={{ cursor: "pointer" }}>
-            <Typography variant="h5" color="inherit">
-              BlogPessoal
+      <AppBar position="static" className="container-navbar">
+        <Toolbar variant="dense" className="container-conteudo">
+          <Box className="container-logo" >
+            <Typography variant="h1" color="inherit" className="titulo">
+              Dario Reis+
             </Typography>
           </Box>
 
-          <Box display="flex" justifyContent="start">
-            <Box mx={1} style={{ cursor: "pointer" }}>
-              <Typography variant="h6" color="inherit">
+          <Box display="flex" justifyContent="start" className="container-menu">
+
+            <Box mx={1}  className="container-conteudo">
+              <Typography variant="h6" color="inherit" className="conteudo-item">
                 home
               </Typography>
             </Box>
-            <Box mx={1} style={{ cursor: "pointer" }}>
-              <Typography variant="h6" color="inherit">
+
+            <Box mx={1}  className="container-conteudo">
+              <Typography variant="h6" color="inherit" className="conteudo-item">
                 postagens
               </Typography>
             </Box>
-            <Box mx={1} style={{ cursor: "pointer" }}>
-              <Typography variant="h6" color="inherit">
+
+            <Box mx={1}   className="container-conteudo">
+              <Typography variant="h6" color="inherit" className="conteudo-item">
                 temas
               </Typography>
             </Box>
-            <Box mx={1} style={{ cursor: "pointer" }}>
-              <Typography variant="h6" color="inherit">
+
+            <Box mx={1}   className="container-conteudo">
+              <Typography variant="h6" color="inherit" className="conteudo-item">
                 cadastrar tema
               </Typography>
             </Box>
-            <Box mx={1} style={{ cursor: "pointer" }}>
-              <Typography variant="h6" color="inherit">
+
+            <Box mx={1}   className="container-conteudo">
+              <Typography variant="h6" color="inherit" className="conteudo-item">
                 logout
               </Typography>
             </Box>
