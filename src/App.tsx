d2dbf95ee846c9cf700e6650559d8c0006167777
Dario Reis <guttyerresjunior1995@gmail.com>
2022-07-29 13:31:03 -0300

    adiciona rota home

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,21 @@ import Home from "./paginas/home/Home";
 import "./App.css";
 import Navbar from "./componets/estaticos/navbar/Navbar";
 import Footer from "./componets/estaticos/footer/Footer";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 
 function App() {
   return (
-    <>
-      <Navbar/>
-      <Home/>
-      <Footer/>
-    </>
-  )
+    <Router>
+      <Navbar />
+      <div style={{ minHeight: "67vh" }}>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </div>
+      <Footer />
+    </Router>
+  );
 }
 
 export default App;
